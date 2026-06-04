e0b81504ed93fac9dc4576a423f02592e6d682c9
thiago <thiago.paz88@gmail.com>
2026-06-03 21:14:58 -0300

    Commit test
    adicionei mais uma espaço

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
             System.out.print("""
 
                     ===MENU===
-                    1. Adicionar Tarefa
+                    1. Adicionar  Tarefa
                     2. Remover Tarefa
                     3. Listar Tarefas
                     4. Sair
